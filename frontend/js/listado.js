2b4672c10072b4723822d3ec0b7e0ee7ec308a5c
baileytorch <baileytorch@hotmail.com>
2026-06-16 14:03:06 -0400

    Procesando DataTables

--- frontend/js/listado.js
+++ frontend/js/listado.js
@@ -1,3 +1,49 @@
-window.onload = function (){
-    new DataTable('#tablaUsuarios');
+window.onload = function () {
+    obtenerUsuarios();
+}
+
+async function obtenerUsuarios() {
+    try {
+        const respuesta = await fetch('http://localhost:3000/obtenerUsuarios');
+        const usuarios = await respuesta.json();
+
+        new DataTable('#tablaUsuarios', {
+            data: usuarios,
+            columns: [
+                { data: 'nombre' },
+                { data: 'correo' },
+                {
+                    data: 'genero',
+                    'render': function (data, type, row) {
+                        let respuesta = '';
+                        switch (data) {
+                            case 'masc':
+                                respuesta = 'Masculino';
+                            case 'fem':
+                                respuesta = 'Femenino';
+                            case 'otro':
+                                respuesta = 'Otro';
+                        }
+                        return respuesta;
+                    }
+                },
+                { data: 'fechaNacimiento' }
+            ]
+        });
+    } catch (error) {
+        console.log('Error al obtener los datos: ', error)
+    }
+}
+
+function cargarGenero(genero) {
+    respuesta = '';
+    switch (genero) {
+        case 'masc':
+            respuesta = 'Masculino';
+        case 'fem':
+            respuesta = 'Femenino';
+        case 'otro':
+            respuesta = 'Otro';
+    }
+    return respuesta;
 }
